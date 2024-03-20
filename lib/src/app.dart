b95b165b4824298eea7d89d7af8209d86675d237
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-20 20:34:13 +0700

    init: first setup project flutter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,3 @@
-import 'package:ecommerce_app/src/screen/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,31 +8,6 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xff673AB7),
-        ),
-        textTheme: const TextTheme(
-          displayMedium: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.normal,
-          ),
-          titleMedium: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.normal,
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.normal,
-          ),
-          labelMedium: TextStyle(
-            fontSize: 10,
-            fontWeight: FontWeight.normal,
-          ),
-        ),
-        fontFamily: GoogleFonts.poppins().fontFamily,
-      ),
-      home: const Home(),
     );
   }
 }
