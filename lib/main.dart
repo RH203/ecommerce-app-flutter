ff52ef48f2189e091adf622335dcc297b1d165bb
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-24 21:42:48 +0700

    feat: add Provider path

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/utils/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(context),
+      child: const App(),
+    ),
+  );
 }
