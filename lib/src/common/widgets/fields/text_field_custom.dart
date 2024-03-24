3bbe7da30f38220630e4c7814f9adaee668c53a9
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-24 16:36:56 +0700

    feat: add common widget text fields

---
+++ lib/src/common/widgets/fields/text_field_custom.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class TextFieldCustom extends StatefulWidget {
+  const TextFieldCustom({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.validator,
+    this.keyboardType = TextInputType.name,
+  }) : isPassword = false;
+
+  const TextFieldCustom.password({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.validator,
+    this.keyboardType = TextInputType.name,
+  }) : isPassword = true;
+
+  final TextEditingController controller;
+  final bool isPassword;
+  final String hintText;
+  final TextInputType keyboardType;
+  final String Function(String?)? validator;
+  @override
+  State<TextFieldCustom> createState() => _TextFieldCustomState();
+}
+
+class _TextFieldCustomState extends State<TextFieldCustom> {
+  bool isVisible = false;
+
+  void onToggleVisibility() {
+    setState(() {
+      isVisible = !isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      keyboardType: widget.keyboardType,
+      decoration: InputDecoration(
+        suffixIcon: widget.isPassword
+            ? GestureDetector(
+                onTap: onToggleVisibility,
+                child: Icon(
+                  isVisible ? Icons.visibility : Icons.visibility_off,
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+              )
+            : null,
+        hintText: widget.hintText,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.surface,
+            width: 2,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.surface,
+            width: 2,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.error,
+            width: 2,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.error,
+            width: 2,
+          ),
+        ),
+        filled: true,
+        fillColor:
+            Theme.of(context).colorScheme.inversePrimary.withOpacity(0.3),
+        contentPadding: const EdgeInsets.all(16),
+      ),
+      obscureText: widget.isPassword && !isVisible,
+      validator: widget.validator,
+    );
+  }
+}
