09e926f30cd76043c0542f2742cf03065f9eb5dd
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-24 21:47:43 +0700

    feat: update provider and fix some color

--- lib/src/utils/theme/theme_provider.dart
+++ lib/src/utils/theme/theme_provider.dart
@@ -1,14 +1,31 @@
+import 'package:ecommerce_app/src/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  
-  bool _darkTheme = false;
+  late ThemeData _themeData;
+  late BuildContext _context; 
 
-  bool get darkTheme => _darkTheme;
-  bool setDarkTheme() => _darkTheme;
+  bool _isDarkMode = false;
 
-  void toggleTheme() {
-    _darkTheme = !_darkTheme;
+  ThemeProvider(BuildContext context) {
+    _context = context; 
+    _themeData = lightColorScheme(context);
+  }
 
+  ThemeData get themeData => _themeData;
+  bool get isDarkMode => _isDarkMode;
+
+  // void setTheme(ThemeData themeData) {
+  //   _themeData = themeData;
+  //   _isDarkMode = _themeData == darkColorScheme(_context); 
+  //   notifyListeners();
+  // }
+
+  void toggleTheme(BuildContext context) {
+    _themeData = _themeData == lightColorScheme(_context)
+        ? darkColorScheme(_context)
+        : lightColorScheme(_context);
+    _isDarkMode = _themeData == darkColorScheme(_context); 
+    notifyListeners();
   }
 }
