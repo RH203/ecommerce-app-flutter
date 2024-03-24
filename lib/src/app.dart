d8b2891a7db4a14c5ba07ecfe1210b04255025d2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-24 21:43:16 +0700

    feat: update color scheme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,10 @@
 import 'package:ecommerce_app/src/features/auth_screen/views/signin_screen.dart';
 import 'package:ecommerce_app/src/features/auth_screen/views/signup_screen.dart';
 import 'package:ecommerce_app/src/features/home_screen/views/home_screen.dart';
-import 'package:ecommerce_app/src/utils/theme/text_theme_custom.dart';
-import 'package:ecommerce_app/src/utils/theme/theme.dart';
+import 'package:ecommerce_app/src/features/main_screen/views/main_screen.dart';
+import 'package:ecommerce_app/src/utils/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -13,24 +14,12 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Ecommerce App",
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: lightColorScheme,
-        textTheme: ThemeData().textTheme.merge(
-              TextThemeCustom.appTextTheme(context),
-            ),
-      ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        colorScheme: darkColorScheme,
-        textTheme: ThemeData().textTheme.merge(
-              TextThemeCustom.appTextTheme(context),
-            ),
-      ),
+      theme: Provider.of<ThemeProvider>(context).themeData,
       routes: {
         '/homescreen': (context) => const HomeScreen(),
         '/signinscreen': (context) => const SignInScreen(),
         '/signupscreen': (context) => const SignUpScreen(),
+        '/mainscreen': (context) => const MainScreen(),
       },
       home: const HomeScreen(),
     );
