9e01831182ccdca973f31ddabf09c8668d3f3975
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-27 10:53:42 +0700

    feat: add IconButton and fix activeSmoothIndicator and Carousel

--- lib/src/features/main_screen/views/main_screen.dart
+++ lib/src/features/main_screen/views/main_screen.dart
@@ -1,8 +1,10 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce_app/src/common/widgets/fields/text_field_custom.dart';
 import 'package:ecommerce_app/src/features/auth_screen/models/validator/validator.dart';
+import 'package:ecommerce_app/src/features/main_screen/models/model_brands.dart';
 import 'package:ecommerce_app/src/utils/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -15,10 +17,16 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  TextEditingController _searchController = TextEditingController();
+  int _activeIndex = 0;
+
+  void _onTapSearch() {
+    String searchValue = _searchController.text;
+    print(searchValue);
+  }
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController _searchController = TextEditingController();
-    int _activeIndex = 0;
     return Consumer<ThemeProvider>(
       builder: (context, themeProvider, child) {
         return Scaffold(
@@ -26,76 +34,129 @@ class _MainScreenState extends State<MainScreen> {
           backgroundColor: Theme.of(context).colorScheme.background,
           appBar: _appBar(context, themeProvider),
           body: Container(
-            margin: const EdgeInsets.all(8),
+            margin: const EdgeInsets.all(10),
             child: Column(
               children: [
-                //* Search bar
-                TextFieldCustom(
-                  controller: _searchController,
-                  hintText: "Search",
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your items";
-                    }
-                    if (Validator.searchIsValid(value)) {
-                      return "Invalid items";
-                    }
-                    return "NULL";
-                  },
-                ),
-                //* Carousel image
-                CarouselSlider(
-                  items: [1, 2, 3, 4, 5].map((i) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 5.0, vertical: 15),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onSecondaryContainer,
-                          ),
-                          child: Center(
-                            child: Text(
-                              'text $i',
-                              style: const TextStyle(fontSize: 16.0),
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  }).toList(),
-                  options: CarouselOptions(
-                    initialPage: 0,
-                    height: 200,
-                    reverse: true,
-                    autoPlay: true,
-                    autoPlayInterval: const Duration(seconds: 4),
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    onPageChanged: (index, reason) {
-                      setState(
-                        () {
-                          _activeIndex = index;
-                        },
-                      );
-                    },
-                  ),
-                ),
-                AnimatedSmoothIndicator(
-                  activeIndex: _activeIndex,
-                  count: 5,
-                  effect: ScrollingDotsEffect(
-                    activeDotColor: Theme.of(context).colorScheme.onBackground,
-                  ),
-                )
+                //! Search bar
+                _searchBarCustom(),
+                //! Carousel image
+                _carouselSlider(),
+                _animatedSmoothIndicator(context),
+                //! Brands
+                _titleBrands(context),
+                _buttonBrands(context),
               ],
             ),
           ),
         );
       },
+    );
+  }
+
+  SizedBox _buttonBrands(BuildContext context) {
+    return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      ...ModelBrands.shoesBrandIcons.map(
+                        (e) {
+                          return Builder(
+                            builder: (context) {
+                              return IconButton(
+                                onPressed: () {},
+                                icon: e,
+                              );
+                            },
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              );
+  }
+
+  Container _titleBrands(BuildContext context) {
+    return Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 7),
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  "Brands",
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                ),
+              );
+  }
+
+  TextFieldCustom _searchBarCustom() {
+    return TextFieldCustom.search(
+                controller: _searchController,
+                hintText: "Search",
+                searchFunction: _onTapSearch,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter your items";
+                  }
+                  if (Validator.searchIsValid(value)) {
+                    return "Invalid items";
+                  }
+                  return "NULL";
+                },
+              );
+  }
+
+  AnimatedSmoothIndicator _animatedSmoothIndicator(BuildContext context) {
+    return AnimatedSmoothIndicator(
+      activeIndex: _activeIndex,
+      count: 5,
+      effect: WormEffect(
+        activeDotColor: Theme.of(context).colorScheme.onBackground,
+      ),
+    );
+  }
+
+  CarouselSlider _carouselSlider() {
+    return CarouselSlider(
+      items: [1, 2, 3, 4, 5].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              child: Center(
+                child: Text(
+                  'text $i',
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
+      options: CarouselOptions(
+        initialPage: 0,
+        height: 180,
+        reverse: true,
+        autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 4),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        onPageChanged: (index, reason) {
+          setState(
+            () {
+              _activeIndex = index;
+            },
+          );
+        },
+      ),
     );
   }
 
@@ -121,7 +182,7 @@ class _MainScreenState extends State<MainScreen> {
             style: Theme.of(context).textTheme.titleLarge!.copyWith(
                   color: Theme.of(context).colorScheme.onBackground,
                 ),
-          )
+          ),
         ],
       ),
       actions: [
