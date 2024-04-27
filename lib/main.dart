2b9cd420a06c46ef3fb4fef41a036b0421f3cdfc
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-04-27 22:59:13 +0700

    feat: init supabase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/utils/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.get('PROJECT_URL'),
+    anonKey: dotenv.get('ANON_KEY_API'),
+  );
+
   runApp(
     MultiProvider(
       providers: [
