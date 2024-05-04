b520e3aaac4be69910b3a8047881d8d567401c53
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-04 19:36:20 +0700

    feat: Refactor FakeStoreApi to remove unused code and update API endpoints

---
+++ lib/src/utils/services/api/products_api.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: unused_field
+
+import 'dart:convert';
+
+import 'package:ecommerce_app/src/features/main_screen/models/models_category_button.dart';
+import 'package:ecommerce_app/src/features/main_screen/models/models_product.dart';
+import 'package:http/http.dart' as http;
+import 'package:logging/logging.dart';
+
+class ProductsApi {
+  // Api Product
+  final String _getAllProductsUrl =
+      "https://raw.githubusercontent.com/RH203/my-static-api/main/basketball_shoes.json";
+  static String imageUrl =
+      "https://raw.githubusercontent.com/RH203/my-static-api/main/";
+  // declare Variabel
+
+  final client = http.Client();
+  ModelsCategoryButton modelsCategoryButton = ModelsCategoryButton();
+  final log = Logger('Services API');
+
+  // Function to get all kinds of products
+  Future<List<ModelsProduct>> getAllProducts() async {
+    try {
+      final response = await http.get(Uri.parse(_getAllProductsUrl));
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = jsonDecode(response.body);
+        List<ModelsProduct> products = jsonData
+            .map((jsonProduct) => ModelsProduct.fromJson(jsonProduct))
+            .toList();
+        return products;
+      } else {
+        throw Exception('Failed to load products: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching products [API]: $e');
+    }
+  }
+}
