f423734d6fdee0b5d2a3d436a277f5558ac7c329
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-05 21:02:25 +0700

    D:ProjectFlutter_projectecommerce_applibsrcfeaturesprofile_screenviews

--- lib/src/features/profile_screen/views/profile_screen.dart
+++ lib/src/features/profile_screen/views/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/src/utils/controller/signout.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -8,18 +9,25 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  SignOut _signOut = SignOut();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Center(
+          child: ElevatedButton(
+        onPressed: () {
+          _signOut.logOutUser();
+          Navigator.popAndPushNamed(context, '/');
+        },
         child: Text(
-          "Profile screen",
-          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+          "Logout",
+          style: Theme.of(context).textTheme.displayLarge!.copyWith(
                 color: Theme.of(context).colorScheme.onBackground,
               ),
         ),
-      ),
+      )),
     );
   }
 }
