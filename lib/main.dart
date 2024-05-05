35d4a73709008b273a8c24e6dc63435cd46727e5
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-05 20:54:22 +0700

    refactor: Refactor main.dart to improve page load with lazy loading for images

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,14 @@ import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
   await Supabase.initialize(
     url: dotenv.get('PROJECT_URL'),
     anonKey: dotenv.get('ANON_KEY_API'),
   );
+
   runApp(
     MultiProvider(
       providers: [
