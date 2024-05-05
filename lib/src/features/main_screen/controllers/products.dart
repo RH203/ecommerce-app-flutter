3c92acb01fe96faedf9d646162a6ba6005a54ccc
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-05 21:00:48 +0700

    refactor: Update Products controller to use logger for logging and improve code readability

--- lib/src/features/main_screen/controllers/products.dart
+++ lib/src/features/main_screen/controllers/products.dart
@@ -1,20 +1,46 @@
 import 'package:ecommerce_app/src/features/main_screen/models/models_product.dart';
 import 'package:ecommerce_app/src/utils/services/api/products_api.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 class Products extends ChangeNotifier {
+  var log = Logger();
   ProductsApi productsApi = ProductsApi();
-  List<ModelsProduct>? _products;
+  List<ModelsProduct> _allProducts = [];
+  List<ModelsProduct> _products = [];
 
   List<ModelsProduct>? get products => _products;
 
   Future<void> getAllProduct() async {
-    // Perubahan 2
+    log.i(_allProducts);
     try {
-      _products = await productsApi.getAllProducts();
+      _allProducts = await productsApi.getAllProducts();
+      _products = List.from(_allProducts);
       notifyListeners();
     } catch (e) {
-      print('Error fetching products [PROVIDER]: $e');
+      log.d('Error fetching products [PROVIDER]: $e');
     }
+  }
+
+  void getAllProductByBrand(String brand) {
+    try {
+      log.i(_allProducts);
+      List<ModelsProduct> filteredProducts = [];
+      for (var temp in _allProducts) {
+        if (temp.brand == brand) {
+          filteredProducts.add(temp);
+        }
+      }
+      _products = filteredProducts;
+      notifyListeners();
+    } catch (e) {
+      log.e(e);
+    }
+  }
+
+  void resetProducts() {
+    log.i(_allProducts);
+    _products = List.from(_allProducts);
+    notifyListeners();
   }
 }
