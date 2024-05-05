ce00c69094f9faf30f8c69ddfa3b456ec94441ac
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-05 20:55:56 +0700

    feat: add initialization when app load.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,34 @@
 import 'package:ecommerce_app/src/features/home_screen/views/home_screen.dart';
+import 'package:ecommerce_app/src/features/main_screen/views/main_screen.dart';
 import 'package:ecommerce_app/src/routes/custom_router.dart';
+import 'package:ecommerce_app/src/utils/shared/shared_prefe.dart';
 import 'package:ecommerce_app/src/utils/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  var log = Logger();
+  bool? _accessToken;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SharedPref sharedPref = SharedPref();
+    sharedPref.getAccessToken().then((value) {
+      setState(() {
+        _accessToken = value;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +38,16 @@ class App extends StatelessWidget {
       theme: Provider.of<ThemeProvider>(context).themeData,
       themeMode: ThemeMode.system,
       onGenerateRoute: (settings) =>
-          CustomRouter.pageRoute(settings.name ?? '/homescreen', settings),
-      home: const HomeScreen(),
+          CustomRouter.pageRoute(settings.name ?? '/', settings),
+      home: _buildInitialScreen(),
     );
+  }
+
+  Widget _buildInitialScreen() {
+    if (_accessToken == true) {
+      return const MainScreen();
+    } else {
+      return const HomeScreen();
+    }
   }
 }
