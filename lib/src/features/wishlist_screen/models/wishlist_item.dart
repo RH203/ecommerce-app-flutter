ff7c424948cc38d2fe2152e8be8a18d8cb5cdea2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-12 00:51:08 +0700

    feat: Add WishlistItem model for wishlist screen functionality

---
+++ lib/src/features/wishlist_screen/models/wishlist_item.dart
@@ -0,0 +1,13 @@
+class WishlistItem {
+  final String itemId;
+  final String title;
+  final String image;
+  final double price;
+
+  WishlistItem({
+    required this.itemId,
+    required this.title,
+    required this.image,
+    required this.price,
+  });
+}
